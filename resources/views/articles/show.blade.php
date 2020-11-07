dade9956fd3769632eb788021a629fddd5b44b1c
vagrant <vagrant@homestead>
2020-11-07 14:13:06 +0000

    用户可以发表评论

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -67,7 +67,7 @@
       {{-- 用户回复列表 --}}
       <div class="card topic-reply mt-4">
           <div class="card-body">
-              @include('articles._reply_box', ['article' => $article])
+              @includeWhen(Auth::check(), 'articles._reply_box', ['article' => $article])
               @include('articles._reply_list', ['replies' => $article->replies()->with('user')->get()])
           </div>
       </div>
